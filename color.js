c36cf5ef0d949b354e5c4245c4a22cb5a028e7b3
mdxqw <sofamedvedeva565@gmail.com>
2025-10-11 06:30:10 +0000

    Sat, Oct 11, 2025, 1:30 PM +07:00

---
+++ color.js
@@ -0,0 +1,10 @@
+ let body = document.querySelector("body");
+        document.querySelector('#red').addEventListener("click",
+        function(){body.style.backgroundColor = "red";}
+    );
+    document.querySelector('#red').addEventListener("click",
+        function(){body.style.backgroundColor = "green";}
+    );
+    document.querySelector('#red').addEventListener("click",
+        function(){body.style.backgroundColor = "blue";}
+    );
